01dabdad06c004e4c1a713c28a1962c03a0d254e
Julian Cohn <juliancohn@gmail.com>
2017-04-10 14:33:50 -0400

    created delete

--- pocket_clone.py
+++ pocket_clone.py
@@ -204,4 +204,4 @@ def extras(a):
         return "error"
 if __name__ == "__main__":
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(debug=True, )
+    app.run(debug=True, host='0.0.0.0')
